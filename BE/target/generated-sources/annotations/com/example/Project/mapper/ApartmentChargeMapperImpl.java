30722b87dcadf631046815c5168ca980504d675c
NamTDno2 <nam0837305039@gmail.com>
2025-02-12 21:26:19 +0700

    up code

--- BE/target/generated-sources/annotations/com/example/Project/mapper/ApartmentChargeMapperImpl.java
+++ BE/target/generated-sources/annotations/com/example/Project/mapper/ApartmentChargeMapperImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-02T06:54:45+0700",
-    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
+    date = "2025-02-12T21:21:46+0700",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250115-2156, environment: Java 21.0.5 (Eclipse Adoptium)"
 )
 @Component
 public class ApartmentChargeMapperImpl implements ApartmentChargeMapper {
